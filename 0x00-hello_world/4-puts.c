2d743c7284c4a9b62908dafe7868b9def0e0a9d5
Gloryproducer <gloryproducer@gmail.com>
2022-09-20 21:35:48 +0000

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.o>
+
+/**
+ * main - print the string with the put fuction
+ * 
+ * program that prints exactly "Programming is like building a multilingual
+ * Return: 0
+ */
+int main(void)
+{ 
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
